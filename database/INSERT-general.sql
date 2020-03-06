3ee863c345941d12152daa1733147e3cf69f0b62
samkent <kent_sam@outlook.com>
2020-03-06 09:41:27 +0000

    Added the insert SQL statements as a file.

---
+++ database/INSERT-general.sql
@@ -0,0 +1,14 @@
+/* [] means the data is optional */
+/* {} means the data is a placeholder */
+
+/* For REGISTER page */
+INSERT INTO user (user_email, [user_display_name,] [user_department,] password_hash, password_salt)
+VALUES ({user_email}, [{user_display_name},] [{user_department},] {password_hash}, {password_salt});
+
+/* For LOG_IN page */
+INSERT INTO user (user_email, password_hash, password_salt)
+VALUES ({user_email}, {password_hash}, {password_salt});
+
+/* for REPORT page */
+INSERT INTO reports (report_type, [report_details,] [reporter_id,] report_date)
+VALUES ({report_type}, [{report_details},] [{reporter_id},] {report_date});

d22defea49a2dcdcb7d37eee84305a1c9acf1b03
Zbigniew Lisak <zbigniew.lisak68@gmail.com>
2020-03-18 22:16:50 +0000

    Added login page view

--- bookmark-site.rb
+++ bookmark-site.rb
@@ -2,15 +2,46 @@
 require 'sinatra'
 require 'bcrypt'
 require_relative 'ruby-scripts/database-model.rb'
+require_relative 'user-authentication.rb'
 
 # For running the server from codio.
 set :bind, '0.0.0.0'
+enable :sessions
 
 Bookmarks.init "../database/test.db"
+
+before do
+    if !session[:user_ID] 
+        session[:user_ID] = -1
+    end
+end
 
 # Redirect to the index view when a general 
 # get connection is opened.
 get '/' do
+    
     @results = Bookmarks.getHomepageDataAll
     erb :index
+end
+
+get '/login' do
+    if session[:user_ID] == -1 
+        erb :loginPage
+    else
+        redirect '/'
+    end
+end
+
+post '/authenticate-user' do
+
+    @login = params[:user_email]
+    @password = params[:user_password]
+
+    session[:user_ID] = UserAuthentication.check @login , @password
+    if session[:user_ID] = -1
+        redirect '/login'
+    else
+        redirect '/'
+    end
+
 end

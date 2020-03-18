c6443357bb059733df2c987177d7960c757430f2
Zbigniew Lisak <zbigniew.lisak68@gmail.com>
2020-03-18 22:16:30 +0000

    Corrected user authentication

--- user-authentication.rb
+++ user-authentication.rb
@@ -11,9 +11,11 @@ module UserAuthentication
         # returns userID if passwords match, -1 otherwise
         validUser = false
         queryResult = Bookmarks.getPasswordHash(email)
-        userID = queryResult[:id]
-        hashCheck = BCrypt::Password.new(queryResult[:password])
-        validUser = hashCheck.is_password?(password)
+        if queryResult
+            userID = queryResult[:id]
+            hashCheck = BCrypt::Password.new(queryResult[:password])
+            validUser = hashCheck.is_password?(password)
+        end
         if validUser then
             return userID
         else

406c425304a2e1b1688e8a1af46cad27e84cecd1
Zbigniew Lisak <zbigniew.lisak68@gmail.com>
2020-04-01 13:08:07 +0200

    Fixed registration

--- ruby-scripts/support-functions.rb
+++ ruby-scripts/support-functions.rb
@@ -11,9 +11,9 @@ def getErrorMessage params
     if ! isValidEmail email
         error_msg += "Please enter valid email <br>"
     elsif email != email_conf
-        error_msg+= "Emails don't match <br>"
+        error_msg += "Emails don't match <br>"
     elsif ! Bookmarks.isUniqueValue "users", "user_email", email
-        error_message+= "Email already in use <br>"
+        error_msg += "Email already in use <br>"
     end
 
     password = params[:password]
@@ -21,7 +21,7 @@ def getErrorMessage params
     if ! isValidPassword password
         error_msg += "Please enter valid password <br>"
     elsif password != password_conf
-         error_msg += "Passwords don't match<br>"
+        error_msg += "Passwords don't match<br>"
     end
 
     return error_msg
@@ -33,7 +33,7 @@ def isValidEmail email
 end
 
 def isValidPassword password
-    return (!password.nil?) && (password.length>=8) && (password.index /\d/ ) && (password.index /[#?!@$%^&*-_]/) && (!password.index /\s/)
+    return (!password.nil?) && (password.length>=8) && !(password.index /\d/).nil? && !(password.index /[\#\?\!\@\$\%\^\&\*\-\_]/).nil? && (password.index /\s/).nil?
 end
 
 def newUser displayName, email, password
@@ -51,7 +51,7 @@ end
 def newBookmark userID, title, link, desc
     title = nil if title == ""
     link = nil if link == ""
-    desc = nil if desc = ""
+    desc = nil if desc == ""
 
     return Bookmarks.addBookmark title, desc, link, userID,  Time.now.strftime("%d/%m/%Y")
 end
@@ -67,3 +67,5 @@ def extractTagsFromParams params
     end
     return ""
 end
+
+puts (isValidPassword "password44_")

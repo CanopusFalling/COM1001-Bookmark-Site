e860819f6e6ffaf1be8ca985b665b0d70234f9b5
aca19sak <sakent1@sheffield.ac.uk>
2020-04-06 22:04:21 +0100

    Finished adding test data in all of the tables.

--- database/insert-test-data.sql
+++ database/insert-test-data.sql
@@ -156,5 +156,41 @@ INSERT INTO views(viewer_ID, bookmark_viewed_ID, view_date) VALUES
 
 INSERT INTO edit(editor_ID, bookmark_edited_ID, edit_date) VALUES
 (1, 1, '2020-02-03'),
-(1, 1, '2020-02-09');
+(2, 1, '2020-02-09');
 
+INSERT INTO favourite(user_ID, bookmark_ID) VALUES
+(1, 1),
+(1, 3),
+(1, 6),
+(1, 8),
+(1, 5),
+(1, 2),
+(1, 10),
+(1, 11),
+(1, 14),
+(1, 17),
+(2, 2),
+(2, 5),
+(2, 7),
+(2, 8),
+(2, 9),
+(2, 10),
+(2, 12),
+(2, 13),
+(2, 14),
+(2, 16);
+
+INSERT INTO tag_bookmark_link(tag_ID, bookmark_ID) VALUES
+(1, 1),
+(1, 2),
+(1, 13),
+(1, 14),
+(2, 2),
+(2, 14),
+(3, 7),
+(3, 8),
+(3, 9),
+(3, 10),
+(3, 11),
+(3, 12),
+(4, 18);
